e43369b5d3710a185da5729715bcf828dcead220
SuyeonSun <suninisy@gmail.com>
2025-03-07 19:05:23 +0900

    navigation style 파일 분리

--- src/components/about/navigation.tsx
+++ src/components/about/navigation.tsx
@@ -1,18 +1,14 @@
 "use client";
 import { Flex, Text } from "@theshop/ui";
 import Link from "next/link";
+import classNames from "classnames/bind";
+import style from "./navigation.module.scss";
+
+const cx = classNames.bind(style);
 
 export default function Navigation() {
   return (
-    <div
-      style={{
-        backgroundColor: "transparent",
-        height: "70px",
-        opacity: 1,
-        alignContent: "center",
-        padding: "0px 20px 0px 20px",
-      }}
-    >
+    <div className={cx({ "navigation-container": true })}>
       <Flex direction="row" justify="between" align="center">
         <Text color="adaptiveBackground" size="body2">
           THE SHOP
